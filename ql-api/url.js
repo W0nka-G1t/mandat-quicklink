e69065611cb40bbdb5984b61cd66555f916bbad9
W0nka-G1t <william@bahyse.ch>
2026-02-17 09:15:14 +0000

    update2.3

--- ql-api/url.js
+++ ql-api/url.js
@@ -20,7 +20,7 @@ document.getElementById('linkForm').addEventListener('submit', async (e) => {
         <div class="success">
           <p>Shortened URL created!</p>
           <p>Short Code: <strong>${data.shortCode}</strong></p>
-          <p>Short URL: <a href="section.click${data.shortUrl}" target="_blank">${data.shortUrl}</a></p>
+          <p>Short URL: <a href="section.click/${data.shortUrl}" target="_blank">${data.shortUrl}</a></p>
           <p>Original URL: ${data.originalUrl}</p>
         </div>
       `;
