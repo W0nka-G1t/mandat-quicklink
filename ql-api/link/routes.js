81759cebccba1549651bd76e75f151d258bae54a
W0nka-G1t <william@bahyse.ch>
2026-02-17 12:08:38 +0000

    am

--- ql-api/link/routes.js
+++ ql-api/link/routes.js
@@ -4,12 +4,14 @@ const IsAuthenticated = require('../common/middlewares/IsAuthenticated');
 const CheckPermission = require('../common/middlewares/CheckPermission');
 
 router.post('/', LinkController.createLink);
-router.get('/:code', LinkController.getLink);
 
 // Admin: voir tous les liens courts créés
+// IMPORTANT: This must come BEFORE /:code route to avoid conflict
 router.get('/all', IsAuthenticated.check, CheckPermission.has('admin'), (req, res, next) => {
   console.log('Route /link/all hit - UserID:', req.user?.userId, 'Username:', req.user?.username);
   next();
 }, LinkController.getAllLinks);
+
+router.get('/:code', LinkController.getLink);
 
 module.exports = router;
